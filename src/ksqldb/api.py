ab4c45fc6f9fe5b9e6f9d47761e134a710b76c66
rushs <rush.song@vecima.com>
2026-01-24 18:14:03 +0800

    make api same with previous version

--- src/ksqldb/api.py
+++ src/ksqldb/api.py
@@ -8,7 +8,8 @@ import requests
 class BaseAPI:
     def __init__(self, url, api_key=None, api_secret=None, https_cfg=None):
         self.url = url
-        self.headers = {"Content-Type": "application/vnd.ksql.v1+json"}
+        self.headers = {"Accept": "application/vnd.ksql.v1+json",
+                        "Content-Type": "application/vnd.ksql.v1+json"}
         if api_key and api_secret:
             b64string = base64.b64encode(bytes(f"{api_key}:{api_secret}"))
             self.headers["Authorization"] = f"Basic {b64string}"
@@ -40,10 +41,12 @@ class BaseAPI:
             "properties": stream_properties if stream_properties else {},
         }
 
-        client = httpx.AsyncClient(http1=False, http2=True, verify=self.verify, cert=self.cert)
+        client = httpx.AsyncClient(http1=False, http2=True, headers=self.headers, verify=self.verify, cert=self.cert)
         async with client.stream("POST", f"{self.url}/query-stream", json=body, timeout=timeout) as r:
             async for line in r.aiter_lines():
-                yield json.loads(line)
+                # Response is a multi-line json array, so remove the array wrapper so that we can
+                # parse individual lines and return them.
+                yield json.loads(line.strip('[],'))
 
     def close_query(self, query_id):
         response = requests.post(f"{self.url}/close-query", json={"queryId": query_id}, verify=self.verify, cert=self.cert)
